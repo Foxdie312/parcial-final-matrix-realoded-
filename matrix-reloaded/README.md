a8595601bc070d3628398def220559087df9126e
foxdie <juegajose.8878@gmail.com>
2024-12-13 16:57:10 -0500

    actualizar archivo

---
+++ matrix-reloaded/README.md
@@ -0,0 +1,6 @@
+Requerimiento 2: Asignar Anomalía
+Descripción: Permitir asignar una anomalía específica a un agente.
+Tareas:
+Crear un método asignarAnomalia(Anomaly anomalia) en la clase Agent.
+Añadir un atributo anomaliasAsignadas (lista de objetos Anomaly) a la clase Agent.
+Mostrar las anomalías asignadas al agente.
--- matrix-reloaded/src/entities/Agente.java
+++ matrix-reloaded/src/entities/Agente.java
@@ -1,12 +1,54 @@
 package entities;
 
+import java.util.Scanner;
 public class Agente extends Entidad {
     private String habilidadEspecial;
     private String misionAsignada;
+    private String clonarse;
+    private String teleport;
+    private String destuirReality;
 
     public Agente(int id, String nombre, String habilidadEspecial) {
         super(id, nombre);
         this.habilidadEspecial = habilidadEspecial;
+    }
+
+    public void asignarAnomalia(String clonarse, String teleport, String destruirReality){
+        this.clonarse = clonarse;
+        this.teleport = teleport;
+        this.destuirReality = destruirReality;
+    }
+
+    public void listaAnomalias(){
+        Scanner escoger = new Scanner(System.in);
+
+        System.out.println("escoja el tipo de anomalia para el agente");
+        System.out.println("1.clonarse");
+        System.out.println("2.teletransporte");
+        System.out.println("3.destruir la realidad");
+
+        Byte asignar = escoger.nextByte();
+        switch (asignar) {
+            case 1:
+                System.out.println("anomalia de agente: ");
+                asignar.asignarAnomalia();
+
+                break;
+            case 2:
+                System.out.println("anomalia de agente: ");
+            break;
+        
+            case 3:
+            System.out.println("anomalia de agente: ");
+            break;
+            default:
+                break;
+        }
+
+    }
+
+    public void mirarAnomalia(){
+        System.out.println("tipo de anomalia: " + );
     }
 
     public String getHabilidadEspecial() {
